dbf3acf24d7f40d71d27c6a2e67f5970bb69b655
Jose Javier <jmesabong@gmail.com>
2020-10-29 15:46:03 -0400

    cambio al titulo del componente Curso

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -6,7 +6,9 @@ const Curso = () => (
             <img src="http://localhost:3000/1.jpg" alt="curso" />
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-            <h3 className="t5 s-mb-2 s-center"> Programación orientada a objetos con Go </h3>
+            <h3 className="t5 s-mb-2 s-center"> 
+                Programación orientada a objetos con Go 
+            </h3>
             <div className="s-mb-2 s-main-center">
                 <div className="card__teacher s-cross-center">
                     <div className="card__avatar s-mr-1">
